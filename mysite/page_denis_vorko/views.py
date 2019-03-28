5fcab71dcc67ee834c9fdc840f5a565e4c882d74
ihoryarosh <hedgegog1@gmail.com>
2019-03-28 21:39:12 +0200

    added zagolovok vorko page

--- mysite/page_denis_vorko/views.py
+++ mysite/page_denis_vorko/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    number = [1,2,3,4,5,6,7,8,9,10]
+    number = [i for i in range(27)]
     return render(request, 'page_denis_vorko/index.html', context={'numbers':number})
